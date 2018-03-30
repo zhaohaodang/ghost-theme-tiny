26c6369e17205a8487471331ad7f66d29644789d
zhaohaodang <zhaohaodang@meituan.com>
2018-03-30 11:01:08 +0800

    视觉升级

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,10 @@
 var tags = document.querySelectorAll('.tag-box')
-// console.log(tags[0].childNodes)
 for (var i = 0, len = tags.length; i < len; i++) {
-    var childNodes = tags[i].childNodes
+    var tag = tags[i]
+    if(!tag){
+        continue;
+    }
+    var childNodes = tag.childNodes
     for (var x = 0, len2 = childNodes.length; x < len2; x++) {
         var node = childNodes[x]
         if (node&&node.nodeType == 3) {
@@ -9,6 +12,3 @@ for (var i = 0, len = tags.length; i < len; i++) {
         }
     }
 }
-// tags[0].removeChild(tags[0].childNodes[0])
-
-console.log(tags[0].childNodes)
